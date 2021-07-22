cefe2e98b8c016197533297ee39e06d7f0b613b8
heeejoo0518 <heeejoo0518@gmail.com>
2021-07-22 19:06:38 +0900

    자동로그인-끝!

--- src/main/java/com/kh/moyoung/member/controller/MemberController.java
+++ src/main/java/com/kh/moyoung/member/controller/MemberController.java
@@ -42,23 +42,10 @@ public class MemberController {
 	private UserMailSendService mailsender;
 	
 	@GetMapping("/signin")
-	public ModelAndView signInView(ModelAndView model, HttpServletRequest request, 
+	public String signInView(ModelAndView model, HttpServletRequest request, 
 			@SessionAttribute(name = "signinMember", required = false) Member signinMember) {
-		if(signinMember != null) model.setViewName("redirect:/");
-		
-		model.setViewName("/member/signin");
-		
-		Cookie[] cookies = request.getCookies();
-		
-		for(Cookie cookie:cookies) {
-			if(cookie.getName().equals("saveId")) {
-				signinMember =  service.autoLogin(cookie.getValue());
-				model.addObject("signinMember", signinMember);
-				model.setViewName("redirect:/");
-			}
-		}
-		
-		return model;
+		if(signinMember != null) return "redirect:/";
+		else return "/member/signin";
 	}
 	
 	@RequestMapping(value = "/signin", method = {RequestMethod.POST})
